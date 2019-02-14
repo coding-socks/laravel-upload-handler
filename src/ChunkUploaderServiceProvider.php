f4518c3118d9d9892d4c6356edf59b3553dbc3c4
yak0d3 <contact.raedyak@gmail.com>
2019-02-14 20:29:05 +0100

    Initial Commit

---
+++ src/ChunkUploaderServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LaraCrafts\ChunkUploader;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChunkUploaderServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
