a4f91289c853bf4421fece1c50d42eee8a01e0ee
Nerg4l <gorog_laci@hotmail.com>
2019-02-19 11:58:45 +0100

    Add general http exceptions

---
+++ src/Exception/ChecksumMismatchHttpException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LaraCrafts\ChunkUploader\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ChecksumMismatchHttpException extends HttpException
+{
+    /**
+     * ChecksumMismatchHttpException constructor.
+     *
+     * @param null $message
+     * @param \Exception|null $previous
+     * @param int $code
+     */
+    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    {
+        parent::__construct(460, $message, $previous, [], $code);
+    }
+}
