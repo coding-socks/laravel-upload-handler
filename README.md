8bea54ff360dc25b3787722d75e3bc85615ecf74
László Görög <contact@nergal.xyz>
2020-06-20 19:38:43 +0200

    Add ng-file-upload driver (#61)
    * Add ng-file-upload driver
    
    * Re-arrange use
    
    * Remove unnecessary line

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ project at the moment is [tus](https://tus.io/).
     - [Blueimp](#blueimp-driver)
     - [DropzoneJS](#dropzonejs-driver)
     - [Flow.js](#flow-js-driver)
+    - [ng-file-upload](#ng-file-upload-driver)
     - [Plupload](#plupload-driver)
     - [Resumable.js](#resumable-js-driver)
     - [simple-uploader.js](#simple-uploader-js-driver)
@@ -153,6 +154,7 @@ Service                                          | Driver name          | Chunk 
 [Blueimp](#blueimp-driver)                       | `blueimp`            | yes          | yes
 [DropzoneJS](#dropzonejs-driver)                 | `dropzone`           | yes          | no
 [Flow.js](#flow-js-driver)                       | `flow-js`            | yes          | yes
+[ng-file-upload](#ng-file-upload-driver)         | `ng-file-upload`     | yes          | no
 [Plupload](#plupload-driver)                     | `plupload`           | yes          | no
 [Resumable.js](#resumable-js-driver)             | `resumable-js`       | yes          | yes
 [simple-uploader.js](#simple-uploader-js-driver) | `simple-uploader-js` | yes          | yes
@@ -178,6 +180,12 @@ This driver handles requests made by the DropzoneJS client library.
 [website](https://github.com/flowjs/flow.js)
 
 This driver handles requests made by the Flow.js client library.
+
+### ng-file-upload driver
+
+[website](https://github.com/danialfarid/ng-file-upload)
+
+This driver handles requests made by the ng-file-upload client library.
 
 ### Plupload driver
 
---
+++ src/Driver/NgFileUploadDriver.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace CodingSocks\ChunkUploader\Driver;
+
+use Closure;
+use CodingSocks\ChunkUploader\Helper\ChunkHelpers;
+use CodingSocks\ChunkUploader\Identifier\Identifier;
+use CodingSocks\ChunkUploader\Range\NgFileUploadRange;
+use CodingSocks\ChunkUploader\Response\PercentageJsonResponse;
+use CodingSocks\ChunkUploader\StorageConfig;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+class NgFileUploadDriver extends UploadDriver
+{
+    use ChunkHelpers;
+
+    /**
+     * @var \CodingSocks\ChunkUploader\Identifier\Identifier
+     */
+    private $identifier;
+
+    /**
+     * NgFileUploadDriver constructor.
+     *
+     * @param \CodingSocks\ChunkUploader\Identifier\Identifier $identifier
+     */
+    public function __construct(Identifier $identifier)
+    {
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(Request $request, StorageConfig $config, Closure $fileUploaded = null): Response
+    {
+        if ($this->isRequestMethodIn($request, [Request::METHOD_GET])) {
+            return $this->resume($request, $config);
+        }
+
+        if ($this->isRequestMethodIn($request, [Request::METHOD_POST])) {
+            return $this->save($request, $config, $fileUploaded);
+        }
+
+        throw new MethodNotAllowedHttpException([
+            Request::METHOD_GET,
+            Request::METHOD_POST,
+        ]);
+    }
+
+    private function resume(Request $request, StorageConfig $config): Response
+    {
+        $request->validate([
+            'file' => 'required',
+            'totalSize' => 'required',
+        ]);
+
+        $originalFilename = $request->get('file');
+        $totalSize = $request->get('totalSize');
+        $uuid = $this->identifier->generateFileIdentifier($totalSize, $originalFilename);
+
+        if (!$this->chunkExists($config, $uuid)) {
+            return new JsonResponse([
+                'file' => $originalFilename,
+                'size' => 0,
+            ]);
+        }
+
+        $chunk = Arr::last($this->chunks($config, $uuid));
+        $size = explode('-', basename($chunk))[1] + 1;
+
+        return new JsonResponse([
+            'file' => $originalFilename,
+            'size' => $size,
+        ]);
+    }
+
+    private function save(Request $request, StorageConfig $config, Closure $fileUploaded = null): Response
+    {
+        $file = $request->file('file');
+
+        $this->validateUploadedFile($file);
+
+        if ($this->isMonolithRequest($request)) {
+            return $this->saveMonolith($file, $config, $fileUploaded);
+        }
+
+        $this->validateChunkRequest($request);
+
+        return $this->saveChunk($file, $request, $config, $fileUploaded);
+    }
+
+    private function isMonolithRequest(Request $request)
+    {
+        return empty($request->post());
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     */
+    private function validateChunkRequest(Request $request): void
+    {
+        $request->validate([
+            '_chunkNumber' => 'required|numeric',
+            '_chunkSize' => 'required|numeric',
+            '_totalSize' => 'required|numeric',
+            '_currentChunkSize' => 'required|numeric',
+        ]);
+    }
+
+    /**
+     * @param \Illuminate\Http\UploadedFile $file
+     * @param \CodingSocks\ChunkUploader\StorageConfig $config
+     * @param \Closure|null $fileUploaded
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function saveMonolith(UploadedFile $file, StorageConfig $config, Closure $fileUploaded = null): Response
+    {
+        $path = $file->store($config->getMergedDirectory(), [
+            'disk' => $config->getDisk(),
+        ]);
+
+        $this->triggerFileUploadedEvent($config->getDisk(), $path, $fileUploaded);
+
+        return new PercentageJsonResponse(100);
+    }
+
+    /**
+     * @param \Illuminate\Http\UploadedFile $file
+     * @param \Illuminate\Http\Request $request
+     * @param \CodingSocks\ChunkUploader\StorageConfig $config
+     * @param \Closure|null $fileUploaded
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function saveChunk(UploadedFile $file, Request $request, StorageConfig $config, Closure $fileUploaded = null): Response
+    {
+        try {
+            $range = new NgFileUploadRange($request);
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage(), $e);
+        }
+
+        $originalFilename = $file->getClientOriginalName();
+        $totalSize = $request->get('_totalSize');
+        $uuid = $this->identifier->generateFileIdentifier($totalSize, $originalFilename);
+
+        $chunks = $this->storeChunk($config, $range, $file, $uuid);
+
+        if (!$range->isLast()) {
+            return new PercentageJsonResponse($range->getPercentage());
+        }
+
+        $targetFilename = $file->hashName();
+
+        $path = $this->mergeChunks($config, $chunks, $targetFilename);
+
+        if ($config->sweep()) {
+            $this->deleteChunkDirectory($config, $uuid);
+        }
+
+        $this->triggerFileUploadedEvent($config->getDisk(), $path, $fileUploaded);
+
+        return new PercentageJsonResponse(100);
+    }
+}
