14691ab1ec5e8cbcfc6693ef8940dfcad198cb64
László Görög <contact@nergal.xyz>
2020-11-15 16:52:08 +0100

    identity: add driver creation test cases

---
+++ tests/IdentityManagerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CodingSocks\UploadHandler\Tests;
+
+use CodingSocks\UploadHandler\Identifier\AuthIdentifier;
+use CodingSocks\UploadHandler\Identifier\NopIdentifier;
+use CodingSocks\UploadHandler\Identifier\SessionIdentifier;
+use CodingSocks\UploadHandler\IdentityManager;
+
+class IdentityManagerTest extends TestCase
+{
+    /**
+     * @var IdentityManager
+     */
+    private $manager;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->manager = new IdentityManager($this->app);
+    }
+
+    public function availableDrivers()
+    {
+        return [
+            'auth' => ['auth', AuthIdentifier::class],
+            'nop' => ['nop', NopIdentifier::class],
+            'session' => ['session', SessionIdentifier::class],
+        ];
+    }
+
+    /**
+     * @dataProvider availableDrivers
+     */
+    public function testDriverCreation($driverName, $expectedInstanceOf)
+    {
+        $driver = $this->manager->driver($driverName);
+        $this->assertInstanceOf($expectedInstanceOf, $driver);
+    }
+
+    public function testDefaultDriver()
+    {
+        $driver = $this->manager->driver();
+        $this->assertInstanceOf(SessionIdentifier::class, $driver);
+    }
+}
